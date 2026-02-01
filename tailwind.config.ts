79d44facf2a1f1ecad2ed536ef53a638ca858aab
Ponysopher <ponysopher@protonmail.com>
2026-02-01 12:09:16 -0500

    Remove uncommented code

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,25 +1,15 @@
-// tailwind.config.ts
-import type { Config } from 'tailwindcss'
+import type { Config } from 'tailwindcss';
 
 const config: Config = {
-  content: [ './app/**/*.{ts,tsx}', './components/**/*.{ts,tsx}' ],
+  content: ['./app/**/*.{ts,tsx}', './components/**/*.{ts,tsx}'],
   theme: {
     extend: {
-      // colors: {
-      //   cyber: {
-      //     bg: '#121212',
-      //     surface: '#1e1e1e',
-      //     primary: '#ff00cc',
-      //     secondary: '#00ffe7',
-      //     purple: '#c084fc',
-      //   },
-      // },
       fontFamily: {
         sans: ['var(--font-orbitron)', 'sans-serif'],
       },
     },
   },
   plugins: [],
-}
+};
 
-export default config
+export default config;

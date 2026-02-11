a4694cd3da8cd9f2442be4a3cf6301bd8d6af88f
Ponysopher <ponysopher@protonmail.com>
2026-02-10 19:48:03 -0500

    Load .env.test enironment vars for tests

--- vitest-configs/vitest.setup.ts
+++ vitest-configs/vitest.setup.ts
@@ -1,4 +1,6 @@
-import 'dotenv/config';
+import dotenv from 'dotenv';
+import path from 'path';
+dotenv.config({ path: path.resolve(process.cwd(), '.env.test') });
 import { beforeAll } from 'vitest';
 
 beforeAll(() => {

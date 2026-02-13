6393c0c273e6ef0f22c48cb912a23ce651605988
Ponysopher <ponysopher@protonmail.com>
2026-02-12 20:55:41 -0500

    Allow injection of connection string

--- prisma/client-handle.ts
+++ prisma/client-handle.ts
@@ -2,8 +2,8 @@ import 'dotenv/config';
 import { PrismaClient } from './generated/prisma/client';
 import { PrismaPg } from '@prisma/adapter-pg';
 
-export function getPrismaClient() {
-  const connectionString = `${process.env.DATABASE_URL}`;
-  const adapter = new PrismaPg({ connectionString });
+export function getPrismaClient(connectionString?: string) {
+  const url = connectionString ?? process.env.DATABASE_URL!;
+  const adapter = new PrismaPg({ connectionString: url });
   return new PrismaClient({ adapter });
 }
